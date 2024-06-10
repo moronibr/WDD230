91a0be1b3b8f1664b7cd9ccc92d836f2917948c7
moronibr <moroni.ramos@gmail.com>
2024-06-10 20:24:33 -0300

    5

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+    const currentYear = new Date().getFullYear();
+    document.getElementById("current-year").textContent = currentYear;
+
+    
+    const lastModified = document.lastModified;
+    document.getElementById("last-modified").textContent = lastModified;
+});
